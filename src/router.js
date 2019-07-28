b47e7156136c48796b0a122ea3f38d3f41addb02
yoshi0518 <nakamura@n-asset.com>
2019-07-28 22:15:16 +0900

    不要ファイル・設定を削除

--- src/router.js
+++ src/router.js
@@ -1,8 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
-import About from './views/About.vue'
-import Test from './views/Test.vue'
+import Home from './views/home.vue'
 
 Vue.use(Router)
 
@@ -14,16 +12,6 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      component: About,
-    },
-    {
-      path: '/test',
-      name: 'test',
-      component: Test,
     },
   ],
 })
