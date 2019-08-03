7127bbbb96b709ad493e586db0e96b19dafb219c
yoshi0518 <nakamura@n-asset.com>
2019-08-03 10:00:58 +0900

    ホーム画面修正

---
+++ src/components/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="menu">
+    <div v-for="menu in menus" :key="menu.id" style="margin-top: 20px; margin-bottom: 20px;">
+      <router-link :to="menu.url">
+        {{ menu.name }}
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  props: ['menus'],
+}
+</script>
---
+++ src/views/home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+    <h1>Home</h1>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <Home :menus="this.menus" />
+  </div>
+</template>
+
+<script>
+import Home from '@/components/Home.vue'
+export default {
+  name: 'home',
+  data() {
+    return {
+      menus: [],
+    }
+  },
+  components: {
+    Home,
+  },
+}
+</script>
